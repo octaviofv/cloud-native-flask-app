87009a4ca5a4b08de9ee3d4991effb6231b43877
Shafique Mahen <128451228+ShafiqueMahen@users.noreply.github.com>
2023-06-21 05:37:26 +0100

    Updated app.py's comments and added QoTD functionality. Added HTML templates, CSS and requirements.txt file

--- app.py
+++ app.py
@@ -1,52 +1,82 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, session
 import requests
 import random
+import datetime
+import secrets
+
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 @app.route('/')
+def random_quote_generator():
+    # Render the 'index.html' template
+    return render_template('index.html')
+
+@app.route('/qotd')
+def quote_of_the_day():
+    # Get the quote of the day from the session
+    quote_of_the_day = session.get('quote_of_the_day')
+
+    # If the quote of the day is not stored in the session, fetch it
+    if quote_of_the_day is None:
+        quote_of_the_day = fetch_quote_of_the_day()
+        session['quote_of_the_day'] = quote_of_the_day
+
+    # Render the 'qotd.html' template with the quote of the day
+    return render_template('qotd.html', quote_of_the_day=quote_of_the_day)
+
+@app.route('/quote')
 def get_quote():
-    # Send a GET request to retrieve the list of genres
+    # Fetch the list of genres from the external API
     response = requests.get('https://quote-garden.onrender.com/api/v3/genres')
-    
-    # Check if the request to fetch genres was successful
+
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
-        # Extract the list of genres from the response
         genres = data['data']
-        # Choose a random genre from the list
         random_genre = random.choice(genres)
     else:
-        # Return an error JSON response if the request to fetch genres failed
         return jsonify(error='Failed to fetch genres')
 
-    # Construct the URL with the random genre to fetch quotes
+    # Fetch a random quote from the selected genre
     quote_url = f"https://quote-garden.onrender.com/api/v3/quotes?genre={random_genre}"
-    
-    # Send a GET request to retrieve quotes for the random genre
     response = requests.get(quote_url)
-    
-    # Check if the request to fetch quotes was successful
+
     if response.status_code == 200:
-        # Parse the JSON response
         quotes = response.json()['data']
-        
-        # Check if there are quotes available for the selected genre
+
         if len(quotes) > 0:
-            # Choose a random quote from the list
             random_quote = random.choice(quotes)
-            # Extract the quote, author, and genre from the selected quote
             quote_text = random_quote['quoteText']
             quote_author = random_quote['quoteAuthor']
             quote_genre = random_quote['quoteGenre']
-            # Return the selected quote as a JSON response
             return jsonify(quote=quote_text, author=quote_author, genre=quote_genre)
         else:
-            # Return an error JSON response if no quotes found for the genre
             return jsonify(error='No quotes found for the genre')
     else:
-        # Return an error JSON response if the request to fetch quotes failed
         return jsonify(error='Failed to fetch quotes')
 
+def fetch_quote_of_the_day():
+    # Fetch the quote of the day for the current date from the external API
+    current_date = datetime.date.today().strftime("%Y-%m-%d")
+    quote_url = f"https://quote-garden.onrender.com/api/v3/quotes/random?date={current_date}"
+    response = requests.get(quote_url)
+
+    if response.status_code == 200:
+        quote_data = response.json()['data']
+
+        if len(quote_data) > 0:
+            first_quote = quote_data[0]
+            quote_text = first_quote['quoteText']
+            quote_author = first_quote['quoteAuthor']
+            quote_genre = first_quote['quoteGenre']
+            return {
+                'quote': quote_text,
+                'author': quote_author,
+                'genre': quote_genre
+            }
+
+    return None
+
 if __name__ == '__main__':
+    # Run the Flask application in debug mode on port 8000
     app.run(debug=True, port=8000, host='0.0.0.0')
